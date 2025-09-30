07c65ade2e3193bf3576b13c1217b37812b48bf0
anna_b <nelson_anna9@hotmail.com>
2025-09-30 14:19:30 -0500

    Refactor event listener callback functions

--- script.js
+++ script.js
@@ -1,79 +1,116 @@
 const numberButtons = document.querySelectorAll("button.number");
 const operatorButtons = document.querySelectorAll("button.operator");
-const equal = document.querySelector("#equal");
 const display = document.querySelector(".display");
 
 let number1 = "";
 let operator = "";
 let number2 = "";
-let expressionArray = [];
+let result = "";
+let haveNumber1 = false;
 
-numberButtons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        if (display.innerText.length < 12) {
-            display.innerText += e.target.value;
+// Decimal button
+document.querySelector(".decimal").addEventListener("click", handleDecimalClick);
+
+function handleDecimalClick() { 
+    if (!haveNumber1) {
+        if (number1 === "") return;
+        if (!number1.includes(".")){
+            number1 += ".";
+            display.innerText = `${number1}`;
+        } 
+    } else if (haveNumber1) {
+        if (number2 === "") return;
+        if (!number2.includes(".")){
+            number2 += ".";
+            display.innerText = `${number1} ${operator} ${number2}`;
+        } 
+    }      
+}
+
+// Positive-negative button
+document.querySelector(".pos-neg").addEventListener("click", handlePosNegClick);
+
+function handlePosNegClick() {
+    if (!haveNumber1) {
+        if (number1 === "") return;
+        if (number1.charAt(0) === "-") {
+            number1 = number1.slice(1);
+            display.innerText = `${number1}`;
+        } else {
+            number1 = "-" + number1;
+            display.innerText = `${number1}`;
         }
-    })
-})
-
-operatorButtons.forEach(button => {
-    button.addEventListener("click", ((e) => {
-        if (display.innerText !== "") {
-            button.classList.add("highlight");
-            createExpressionArray(display.innerText, e.target.value);
-            display.innerText = "";
-            equal.classList.remove("highlight");
+    } else if (haveNumber1) {
+        if (number2 === "") return;
+        if (number2.charAt(0) === "-") {
+            number2 = number2.slice(1);
+            display.innerText = `${number2}`;
+        } else {
+            number2 = "-" + number2;
+            display.innerText = `${number1} ${operator} ${number2}`;
         }
-    }))
-})
+    }      
+}
 
-equal.addEventListener("click", function() {
-    if (expressionArray.length === 2) {
-        equal.classList.add("highlight");
-        value = display.innerText;
-        display.innerText = "";
-        createExpressionArray(value, "=");
-        clearOperatorHighlight();
-    };
-});
+// Percent button
+document.querySelector(".percent").addEventListener("click", handlePercentClick);
 
-// decimal button
-document.querySelector(".decimal").addEventListener("click", function() {
-    if (!display.innerText.includes(".")){
-        display.innerText += ".";
-        equal.classList.remove("highlight");
-    }    
-});
-
-// positive-negative button
-document.querySelector(".pos-neg").addEventListener("click", function() {
-    if (display.innerText.charAt(0) === "-") {
-        display.innerText = display.innerText.slice(1);
-    } else {
-        display.innerText = "-" + display.innerText;
+function handlePercentClick(){
+    if (!haveNumber1) {
+        if (number1 === "") return;
+        number1 = (number1 / 100).toString();
+        display.innerText = `${number1}`;
+    } else if (haveNumber1) {
+        if (number2 === "") return;
+        number2 = (number2 / 100).toString();
+        display.innerText = `${number1} ${operator} ${number2}`;
     }
-    equal.classList.remove("highlight");
-});
+}
 
-// percent button
-document.querySelector(".percent").addEventListener("click", function(){
-    display.innerText = display.innerText / 100;
-    equal.classList.remove("highlight");
-});
+// Backspace button
+document.querySelector(".backspace").addEventListener("click", handleBackspace);
 
-// backspace button
-document.querySelector(".backspace").addEventListener("click", function(){
-    display.innerText = display.innerText.slice(0, display.innerText.length - 1);
-    equal.classList.remove("highlight");
+function handleBackspace(){
+    if (!haveNumber1) {
+        if (number1 === "") return;
+        number1 = number1.slice(0, number1.length - 1);
+        display.innerText = `${number1}`;
+    } else if (haveNumber1) {
+        if (number2 === "") return;
+        number2 = number2.slice(0, number2.length - 1);
+        display.innerText = `${number1} ${operator} ${number2}`;
+    }
+}
+
+// Clear button
+document.querySelector(".clear").addEventListener("click", handleClear);
+
+function handleClear() {
+    display.innerText = "";
+    number1 = "";
+    operator = "";
+    number2 = "";
+    result = "";
+    haveNumber1 = false;
+}
+
+// Number buttons
+numberButtons.forEach(button => {
+    button.addEventListener("click", handleNumberButtonClick)
 })
 
-// clear button
-document.querySelector(".clear").addEventListener("click", function() {
-    display.innerText = "";
-    clearOperatorHighlight();
-    equal.classList.remove("highlight")
-    reset();
-});
+function handleNumberButtonClick(e) {
+    clearOperatorHighlight()
+    if (display.innerText.length >= 15) return;
+    const value = e.target.value;
+    if (!haveNumber1) {
+        number1 += value;
+        display.innerText = `${number1}`;
+    } else if (haveNumber1) {
+        number2 += value;
+        display.innerText = `${number1} ${operator} ${number2}`;
+    }           
+}
 
 function clearOperatorHighlight() {
         operatorButtons.forEach(button => {
@@ -81,46 +118,72 @@ function clearOperatorHighlight() {
         })
 };
 
-function createExpressionArray(value, sign){
-    if (number1 === "") {
-        number1 = value;
-        expressionArray.push(number1);
-    } else {
-        number2 = value;
-        expressionArray.push(number2)
-    };
-    if (operator === "" && sign === "+" || sign === "-" || sign === "*" || sign === "/") {
-        operator = sign;
-        expressionArray.push(operator);
-    } else {
-        calculate(expressionArray);
-    };
-};
+// Operator buttons
+operatorButtons.forEach(button => {
+    button.addEventListener("click", handleOperatorButtonClick)
+})
 
-function calculate(array) {
-    if (array[1] === "+") {
-        display.innerText = Number(array[0]) + Number(array[2]);
-        reset()
-    } else if (array[1] === "-") {
-        display.innerText = Number(array[0]) - Number(array[2]);
-        reset()
-    } else if (array[1] === "*") {
-        display.innerText = Number(array[0]) * Number(array[2]);
-        reset()
-    } else if (array[1] === "/") {
-        if (Number(array[2]) === 0) {
-            display.innerText = "Do you want to have an existential crisis?";
-            reset()
+function handleOperatorButtonClick(e)  {
+    if (!haveNumber1) { 
+        if (number1 === "") return; 
+        if (e.target.value === "=") return;
+        operator = e.target.value;
+        display.innerText = `${number1} ${operator}`;
+        haveNumber1 = true;
+        const button = e.target
+        button.classList.add("highlight")   
+    } else if (haveNumber1) {
+        if (number2 === "") return;
+        const number3 = operate(Number(number1), operator, Number(number2))
+        result = formatResultForDisplay(number3)
+        const button = e.target
+        button.classList.add("highlight")
+        if (e.target.value === "=") {
+            number1 = "";
+            operator = "";
+            number2 = "";
+            haveNumber1 = false;
+            display.innerText = `${result}`;
         } else {
-            display.innerText = Number(array[0]) / Number(array[2]);
-            reset()
-        }    
+            number1 = result;
+            operator = e.target.value;
+            number2 = "";
+            haveNumber1 = true;
+            display.innerText = `${result} ${operator}`;
+        }
     }
 }
 
-function reset () {
-    number1 = "";
-    operator = "";
-    number2 = "";
-    expressionArray = [];
-};
+function formatResultForDisplay(num, maxLength = 15) {
+    let str = parseFloat(num.toPrecision(10)).toString(); 
+    if (str.length <= maxLength) return str;
+}
+
+function operate(number1, operator, number2) {
+    if (operator === "+") {
+        return addition(number1, number2);
+    } else if (operator === "-") {
+        return subtraction(number1, number2);
+    } else if (operator === "*") {
+        return multiplication(number1, number2);
+    } else if (operator === "/") {
+        return division(number1, number2);
+    }
+}
+
+function addition (number1, number2) {
+    return number1 + number2;
+}
+function subtraction (number1, number2) {
+    return number1 - number2;
+}
+function multiplication (number1, number2) {
+    return number1 * number2;
+}
+function division (number1, number2) {
+    if (number2 === 0) {
+        return "ERROR";
+    } else {
+        return number1 / number2;
+    }
+}
